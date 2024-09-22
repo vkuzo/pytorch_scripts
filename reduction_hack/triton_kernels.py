3f60912e8b2f0ca722e1a71c9c1a93f1a52674e2
vasiliy <vasiliy@fb.com>
2024-09-21 23:33:33 -0700

    hack to explore performance of triton max with atomics
    Summary:
    
    baseline 1: PyTorch eager mode max
    baseline 2: torch.compile max, two stage at large problem sizes
    experiment: triton max with atomics, one stage
    
    Test Plan:
    
    https://gist.github.com/vkuzo/404ec19d53828ff810e6bce307feeea6
    
    note that at small to medium shapes, the kernel to allocate the tensor
    of a single zero takes a high % of the time, so results may differ in a
    benchmark setup where the GPU is more saturated.
    TODO(later): investigate ^
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

---
+++ reduction_hack/triton_kernels.py
@@ -0,0 +1,43 @@
+"""
+A simple triton kernel to calculate max(tensor) with atomics. This avoids the
+need to do a two-stage reduction.
+"""
+
+import torch
+import triton
+from triton import language as tl
+from triton.runtime import driver
+
+@triton.autotune(
+    configs=[
+        triton.Config({'BLOCK_SIZE': 512}),
+        triton.Config({'BLOCK_SIZE': 1024}),
+        triton.Config({'BLOCK_SIZE': 2048}),
+        triton.Config({'BLOCK_SIZE': 8192}),
+        triton.Config({'BLOCK_SIZE': 16384}),
+    ],
+    key=['n_elements'],
+)
+@triton.jit
+def max_with_atomics_kernel(
+    in_ptr0,
+    out_ptr,
+    n_elements,
+    BLOCK_SIZE: "tl.constexpr",
+):
+    pid = tl.program_id(axis=0)
+    block_start = pid * BLOCK_SIZE
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    mask = offsets < n_elements
+    x = tl.load(in_ptr0 + offsets, mask=mask)
+    x_max = tl.max(x)
+    tl.atomic_max(out_ptr, x_max)
+        
+
+def max_with_atomics(x: torch.Tensor):
+    # Note: need to initialize to zero for numerical correctness of max
+    output = torch.zeros(1, dtype=x.dtype, device=x.device)
+    n_elements = x.numel()
+    grid = lambda meta: (triton.cdiv(n_elements, meta["BLOCK_SIZE"]),)
+    max_with_atomics_kernel[grid](x, output, n_elements)
+    return output.squeeze()
