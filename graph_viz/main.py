391bc70c0b6ff971795d024f081a8ab850bbc06e
Vasiliy Kuznetsov <vasiliy@meta.com>
2025-01-03 09:58:22 -0800

    remove triton fwd/bwd with aten ops only
    note: triton region graphs are more useful, leaving those

--- graph_viz/main.py
+++ graph_viz/main.py
@@ -22,6 +22,7 @@ test_fname3 = 'test_inputs/input_aot_joint_graph_aot_graphs_output_code.txt'
 @dataclass
 class Node:
     node_name: str
+    # TODO(future): move this to metadata
     func: str
     args: List[str]
     metadata: str
@@ -34,22 +35,11 @@ class Node:
 #     ...
 #   },
 #   outputs: ['output0, ...]
-#   triton_kernel_str: """
-# @triton_heuristics.reduction(...
-#   ...
-# ''', device_str='cuda')
-#   """
-#   triton_kernel_type: reduction,
-#   triton_kernel_name: 'triton_red_fused_abs_max_0',
 @dataclass
 class ParsedGraph:
     inputs: List[str] = field(default_factory=lambda: [])
     nodes: Dict[str, Node] = field(default_factory=lambda: {})
     outputs: List[str] = field(default_factory=lambda: [])
-    triton_kernel_str: Optional[str] = None
-    # pointwise/reduction/persistent_reduction
-    triton_kernel_type: Optional[str] = None
-    triton_kernel_name: Optional[str] = None
 
 
 def parse_graph(
@@ -139,102 +129,6 @@ def parse_graph(
 
     return g
 
-def parse_triton_kernel_graph(
-    list_of_lines: List[str],
-    start_idx: int,
-    end_idx: int,
-):
-    g = ParsedGraph()
-
-    for line in list_of_lines[start_idx:end_idx+1]:
-        # %mul : [num_users=1] = call_function[target=torch.ops.aten.mul.Tensor](args = (%reciprocal, 448.0), kwargs = {})
-        # TODO also parse kwargs
-        triton_graph_re = re.compile('#   (\%.*)+ : .*args = \((.*)\), kwargs.*')
-        res = triton_graph_re.search(line)
-        if res:
-            target_node = res.group(1).replace('%', '')
-            args = res.group(2).split(', ')
-            args = [s.replace('%', '').replace(',', '') for s in args]
-
-            g.nodes[target_node] = Node(target_node, '', args, None, node_type='args')
-            # TODO future parse the func as well
-
-    # add the triton kernel text to the graph as metadata, so we can display it later
-    # also add the kernel type and name
-    triton_kernel_start_re = re.compile('triton_heuristics.*')
-    triton_kernel_end_re = re.compile("''', device_str='cuda'.*")
-    triton_kernel_name_re = re.compile('(.*) = async_compile.*')
-    triton_kernel_start_idx, triton_kernel_end_idx = None, None
-    cur_idx = end_idx
-    while True:
-        line = list_of_lines[cur_idx]
-        if triton_kernel_start_re.search(line):
-            triton_kernel_start_idx = cur_idx
-            g.triton_kernel_type = line.replace('@triton_heuristics.', '').replace('(', '')
-        elif triton_kernel_end_re.search(line):
-            triton_kernel_end_idx = cur_idx
-            break
-        cur_idx += 1
-
-        res = triton_kernel_name_re.search(line)
-        if res:
-            g.triton_kernel_name = res.group(1)
-    g.triton_kernel_str = ''.join(list_of_lines[triton_kernel_start_idx:triton_kernel_end_idx])
-
-    # populate the inputs and outputs by:
-    # 1. for each node n, count the number of parent and children nodes
-    # 2. for each node n with 0 parents, add it to inputs
-    # 3. for each node n with 0 children, add it to outputs
-    # note that for joint/fwd/bwd graph we parse inputs/outputs from the graph
-    # printout, but for triton kernels we only have the graph fragment so we
-    # have to resort to this hack
-
-    # B: '', [A]
-    # C: '', [A, B]
-    # D: '', [C]
-    # traversing B: A add one child, B add one parent
-    # traversing C: A and B add one child, C add one parent
-    # traversing D: C add one child, D add one parent
-
-    # TODO(future): make this also handle kwargs
-    node_name_to_num_parents_children = defaultdict(lambda: [0, 0])
-    for node_name, node in g.nodes.items():
-        func = node.func
-        args = node.args
-        metadata = node.metadata
-        for arg in args:
-            # increment child counter
-            node_name_to_num_parents_children[arg][1] += 1
-        # increment parent counter
-        node_name_to_num_parents_children[node_name][0] += len(args)
-
-    inputs = []
-    outputs = []
-    starts_with_az = re.compile('^[a-zA-Z]+.*')
-    for node_name, (num_parents, num_children) in node_name_to_num_parents_children.items():
-
-        # Currently args can be variables (foo_1), numbers (1e12), torch constants (torch.bfloat16), etc
-        # we don't know which one because this is all done with string parsing. For now,
-        # do a hacky filter of vars only by restricting the first character to be a-zA-Z, and discarding
-        # the things that look like `torch.`.  This is pretty brittle.
-        if not(
-            starts_with_az.search(node_name)
-            and not node_name.startswith('torch')
-            and not node_name == 'None'
-        ):
-            continue
-        if num_parents == 0:
-            inputs.append(node_name)
-        elif num_children == 0:
-            outputs.append(node_name)
-
-    g.inputs = inputs
-    g.outputs = outputs
-
-    # verify captured information is valid
-    assert len(g.nodes) > 0
-    return g
-
 def parse_triton_region_graph(
     list_of_lines: List[str],
     start_idx: int,
@@ -277,8 +171,18 @@ def call(args):
     kernel_name_to_num_calls = defaultdict(int)
 
     has_matched_final_return = False
+    cur_kernel_type = None
+
+    kernel_name_to_kernel_type = defaultdict(str)
 
     for line in list_of_lines[start_idx:end_idx+1]:
+
+        # parse the kernel type
+        # @triton_heuristics.reduction(
+        triton_kernel_type_re = re.compile('@triton_heuristics\.(.*)\(')
+        res = triton_kernel_type_re.match(line)
+        if res:
+            cur_kernel_type = res.group(1)
 
         # parse the individual kernel definition lines, we'll use this to map from buffer to input vs output
         individual_kernel_def_re = re.compile('def (triton_.*)\((.*)\)')
@@ -293,7 +197,9 @@ def call(args):
                     arg_idx_to_category[arg_idx] = 'out_ptr'
                 else:
                     arg_idx_to_category[arg_idx] = None
-            kernel_name_to_arg_idx_to_input_output_none[res.group(1)] = arg_idx_to_category
+            kernel_name = res.group(1)
+            kernel_name_to_arg_idx_to_input_output_none[kernel_name] = arg_idx_to_category
+            kernel_name_to_kernel_type[kernel_name] = cur_kernel_type
 
         # primals_1, primals_2 = args
         args_re = re.compile('[ ]+(.*) = args$')
@@ -337,7 +243,14 @@ def call(args):
             kernel_name_to_num_calls[kernel_name] += 1
             cur_kernel_node_name = f'{kernel_name}__{cur_kernel_num_calls}'
 
-            g.nodes[cur_kernel_node_name] = Node(cur_kernel_node_name, '', cur_node_inputs, '', node_type='func')
+            kernel_type = kernel_name_to_kernel_type[kernel_name]
+            g.nodes[cur_kernel_node_name] = Node(
+                cur_kernel_node_name,
+                '',
+                cur_node_inputs,
+                metadata=kernel_type,
+                node_type='func',
+            )
             for cur_node_output in cur_node_outputs:
                 g.nodes[cur_node_output] = Node(cur_node_output, '', [cur_kernel_node_name], '', node_type='args')
 
@@ -429,75 +342,65 @@ def shorten_func_name(s):
     return s
 
 def create_diagram(
-    graphs,
+    g,
     output_dir,
     out_filename,
-    triton_idxs_and_graphs=None,
 ):
     print('creating diagram', output_dir, out_filename)
     dot = Digraph(comment='aot_joint_graph')
     dot.attr(label=out_filename)
     dot.attr(labelloc='t')
+    g_idx = 0
 
-    for g_idx, g in enumerate(graphs):
+    dot.attr(label=f"label: {str(g_idx)}")
 
-        # note: subgraph name must start with cluster_ for graphviz to render a border
-        with dot.subgraph(name=f"cluster_{str(g_idx)}") as c:
+    # 'b' is label at the bottom of subgraph, 't' is at top
+    dot.attr(labelloc='b')
 
-            # TODO(future): render `g.triton_kernel_str` if present
-            if g.triton_kernel_name is not None:
-                label = f"idx: {g_idx}\nkernel_name: {g.triton_kernel_name}\ntype:{g.triton_kernel_type}"
-                c.attr(label=label)
-            else:
-                c.attr(label=f"label: {str(g_idx)}")
+    # Add the start nodes
+    if len(g.inputs):
+        dot.node('input', 'input', shape='oval')
+    for input_name in g.inputs:
+        input_name_with_idx = f"{g_idx}_{input_name}"
+        dot.node(input_name_with_idx, input_name, shape='oval')
+        dot.edge('input', input_name_with_idx, '', color='lightgray')
 
-            # 'b' is label at the bottom of subgraph, 't' is at top
-            c.attr(labelloc='b')
+    # Add the intermediate nodes
+    for node_name, node in g.nodes.items():
+        func_name = node.func
+        args = node.args
+        metadata = node.metadata
+        node_type = node.node_type
+        node_name_with_idx = f"{g_idx}_{node_name}"
+        if metadata is None or metadata == '':
+            metadata='a'
 
-            # Add the start nodes
-            if len(g.inputs):
-                dot.node('input', 'input', shape='oval')
-            for input_name in g.inputs:
-                input_name_with_idx = f"{g_idx}_{input_name}"
-                c.node(input_name_with_idx, input_name, shape='oval')
-                dot.edge('input', input_name_with_idx, '', color='lightgray')
+        metadata_str = f"<font color='red'>{metadata}</font>"
 
-            # Add the intermediate nodes
-            for node_name, node in g.nodes.items():
-                func_name = node.func
-                args = node.args
-                metadata = node.metadata
-                node_type = node.node_type
-                node_name_with_idx = f"{g_idx}_{node_name}"
-                if metadata is None or metadata == '':
-                    metadata='a'
+        # Add the node
+        # use HTML syntax to make things easier to read
+        node_comment = f"<{shorten_func_name(func_name)}({args})<br/><br/>{node_name}<br/>{metadata_str}>"
+        if node_type == 'args':
+            node_comment = node_name
 
-                metadata_str = f"<font color='red'>{metadata}</font>"
+        shape = 'oval'
+        if node_type == 'func':
+            shape = 'rectangle'
 
-                # Add the node
-                # use HTML syntax to make things easier to read
-                node_comment = f"<{shorten_func_name(func_name)}({args})<br/><br/>{node_name}<br/>{metadata_str}>"
-                if node_type == 'args':
-                    node_comment = node_name
+        dot.node(node_name_with_idx, node_comment, shape=shape, color='black')
 
-                shape = 'oval'
-                if node_type == 'func':
-                    shape = 'rectangle'
+        # Add edges to args
+        for arg_name in args:
+            if arg_name in g.inputs or arg_name in g.nodes:
+                arg_name_with_idx = f"{g_idx}_{arg_name}"
+                dot.edge(arg_name_with_idx, node_name_with_idx, '')
 
-                c.node(node_name_with_idx, node_comment, shape=shape, color='black')
-
-                # Add edges to args
-                for arg_name in args:
-                    if arg_name in g.inputs or arg_name in g.nodes:
-                        arg_name_with_idx = f"{g_idx}_{arg_name}"
-                        dot.edge(arg_name_with_idx, node_name_with_idx, '')
-
-            # Create output
-            if len(g.outputs):
-                dot.node('output', 'output', shape='oval')
-            for output_name in g.outputs:
-                output_name_with_idx = f"{g_idx}_{output_name}"
-                dot.edge(output_name_with_idx, 'output', '', color='lightgray')
+    # Create output
+    if len(g.outputs):
+        dot.node('output', 'output', shape='oval')
+    for output_name in g.outputs:
+        output_name_with_idx = f"{g_idx}_{output_name}"
+        dot.edge(output_name_with_idx, 'output', '', color='lightgray')
 
     out_filename = os.path.join(output_dir, out_filename)
     dot.render(out_filename, format='svg', cleanup=True)
@@ -565,19 +468,6 @@ def run(
 
         cur_captured_graph_id = None
         joint_start_idx, forward_start_idx, backward_start_idx, end_idx = None, None, None, None
-
-        # for individual triton kernels
-        triton_start_idx, triton_end_idx = None, None
-
-        # {
-        #   '0': {
-        #     'forward': [[123, 126, None], [128, 134, None], ...],
-        #     'backward': [[245, 256, None], ...]
-        #   },
-        # }
-        #
-        # The `None` entries are for the parsed graphs, which will be filled in later
-        graph_id_to_fwdbwd_to_triton_idxs_and_graphs = defaultdict(lambda: defaultdict(list))
 
         cur_aot_id, cur_fwd_bwd = None, None
 
@@ -660,20 +550,6 @@ def run(
                 cur_fwd_bwd = fwd_or_bwd
                 aot_triton_region_start_idx = idx
 
-            # Start of triton kernel graph fragment
-            # Graph fragment:
-            start_of_triton_graph_fragment = re.compile('.*Graph fragment.*')
-            if start_of_triton_graph_fragment.match(line):
-                triton_start_idx = idx
-
-            # End of triton kernel graph fragment
-            # triton_red_fused_abs_max_0 = async_compile.triton('triton_red_fused_abs_max_0', '''
-            end_of_triton_graph_fragment = re.compile('.* = async_compile.triton.*')
-            if end_of_triton_graph_fragment.match(line):
-                triton_end_idx = idx
-
-                graph_id_to_fwdbwd_to_triton_idxs_and_graphs[cur_aot_id][cur_fwd_bwd].append([triton_start_idx, triton_end_idx, None])
-
             # End of triton kernel file corresponding to AOT ID
             # torch._inductor.codecache.__output_code:Output code written to: /tmp/torchinductor_vasiliy/tmpspdynggy/at/cathtx22pafsaeak2pf2g55j64arrziidjtpayap4wm6hlgbjx5m.py
             end_of_triton_kernel_file = re.compile('.*Output code written to.*')
@@ -697,20 +573,6 @@ def run(
         start_idx, end_idx = aot_graph_id_to_graphs['0']['backward']
         backward_graph = parse_graph(lines, start_idx, end_idx)
 
-    # graph of a triton kernel file, with nodes being the aten node names from
-    # graph fragments
-    if 'forward' in graph_id_to_fwdbwd_to_triton_idxs_and_graphs['0']:
-        for entry in graph_id_to_fwdbwd_to_triton_idxs_and_graphs['0']['forward']:
-            start_idx, end_idx, _ = entry
-            triton_graph = parse_triton_kernel_graph(lines, start_idx, end_idx)
-            entry[2] = triton_graph
-    if 'backward' in graph_id_to_fwdbwd_to_triton_idxs_and_graphs['0']:
-        for entry in graph_id_to_fwdbwd_to_triton_idxs_and_graphs['0']['backward']:
-            start_idx, end_idx, _ = entry
-            triton_graph = parse_triton_kernel_graph(lines, start_idx, end_idx)
-            entry[2] = triton_graph
-    # TODO(future): also backward
-
     # graph of a triton kernel file, with nodes being the buffers passed around
     # in the triton kernel wrapper
     triton_region_graph = None
@@ -727,52 +589,36 @@ def run(
 
     if joint_graph is not None:
         create_diagram(
-            [joint_graph],
+            joint_graph,
             output_dir,
             'joint',
         )
         graph_titles.append('joint')
     if forward_graph is not None:
         create_diagram(
-            [forward_graph],
+            forward_graph,
             output_dir,
             'forward',
-            triton_idxs_and_graphs=graph_id_to_fwdbwd_to_triton_idxs_and_graphs['0']['forward']
         )
         graph_titles.append('forward')
     if backward_graph is not None:
         create_diagram(
-            [backward_graph],
+            backward_graph,
             output_dir,
             'backward',
         )
         graph_titles.append('backward')
 
-    triton_forward_graphs = [g for _, __, g in graph_id_to_fwdbwd_to_triton_idxs_and_graphs['0']['forward']]
-    create_diagram(
-        triton_forward_graphs,
-        output_dir,
-        'triton_forward',
-    )
-    graph_titles.append('triton_forward')
-    triton_backward_graphs = [g for _, __, g in graph_id_to_fwdbwd_to_triton_idxs_and_graphs['0']['backward']]
-    create_diagram(
-        triton_backward_graphs,
-        output_dir,
-        'triton_backward',
-    )
-    graph_titles.append('triton_backward')
-
     if triton_region_graph_forward is not None:
         create_diagram(
-            [triton_region_graph_forward],
+            triton_region_graph_forward,
             output_dir,
             'triton_region_forward',
         )
         graph_titles.append('triton_region_forward')
     if triton_region_graph_backward is not None:
         create_diagram(
-            [triton_region_graph_backward],
+            triton_region_graph_backward,
             output_dir,
             'triton_region_backward',
         )
