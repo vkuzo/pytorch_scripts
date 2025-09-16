804672d32821c660886afc737df23352e4adcb46
vasiliy <vasiliy@fb.com>
2025-09-16 06:25:28 -0700

    annotate dp2ep rest of token dispatch
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- dp2ep/utils.py
+++ dp2ep/utils.py
@@ -4,3 +4,8 @@ def print0(*args, **kwargs):
     if not get_rank() == 0:
         return
     print(*args, **kwargs)
+
+def print1(*args, **kwargs):
+    if not get_rank() == 1:
+        return
+    print(*args, **kwargs)
