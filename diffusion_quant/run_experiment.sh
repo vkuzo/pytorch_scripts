137a0200f81ce2d6fb1b0933554184efa0c52c76
vasiliy <vasiliy@fb.com>
2025-12-15 05:47:34 -0800

    updates to diffusion quant
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- diffusion_quant/run_experiment.sh
+++ diffusion_quant/run_experiment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-with-proxy python diffusion_quant/main.py --mode full_quant --model flux --prompt_set calibration 2>&1 | tee diffusion_quant/outputs/flux/full_quant_fp8_calibration_log.txt
-with-proxy python diffusion_quant/main.py --mode use_sweep_results --model flux --prompt_set calibration 2>&1 | tee diffusion_quant/outputs/flux/use_sweep_results_fp8_calibration_log.txt
-with-proxy python diffusion_quant/main.py --mode full_quant --model flux --prompt_set test 2>&1 | tee diffusion_quant/outputs/flux/full_quant_fp8_test_log.txt
-with-proxy python diffusion_quant/main.py --mode use_sweep_results --model flux --prompt_set test 2>&1 | tee diffusion_quant/outputs/flux/use_sweep_results_fp8_test_log.txt
+with-proxy python diffusion_quant/main.py --mode full_quant --model flux-2.dev --prompt_set calibration 2>&1 | tee diffusion_quant/outputs/flux-2.dev/full_quant_fp8_calibration_log.txt
+with-proxy python diffusion_quant/main.py --mode use_sweep_results --model flux-2.dev --prompt_set calibration 2>&1 | tee diffusion_quant/outputs/flux-2.dev/use_sweep_results_fp8_calibration_log.txt
+with-proxy python diffusion_quant/main.py --mode full_quant --model flux-2.dev --prompt_set test 2>&1 | tee diffusion_quant/outputs/flux-2.dev/full_quant_fp8_test_log.txt
+with-proxy python diffusion_quant/main.py --mode use_sweep_results --model flux-2.dev --prompt_set test 2>&1 | tee diffusion_quant/outputs/flux-2.dev/use_sweep_results_fp8_test_log.txt
